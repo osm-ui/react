3fb15cbeac816fed0d47d260d441a797a02579c2
PanierAvide <panieravide@riseup.net>
2017-03-29 16:42:43 +0200

    Map component: locate control added (see #1)

--- components/map.jsx
+++ components/map.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Map as LeafletMap } from 'react-leaflet';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
+import 'font-awesome/css/font-awesome.css';
+import 'leaflet.locatecontrol';
 
 
-const Map = props => (
-    <LeafletMap {...props}>
-        {props.children}
-    </LeafletMap>
-);
+class Map extends LeafletMap {
+	componentDidMount() {
+		super.componentDidMount();
+		
+		if(this.props.locate === true) {
+			L.control.locate().addTo(this.leafletElement);
+		}
+	}
+}
 
 
 Map.propTypes = {
     ...LeafletMap.propTypes,
+	locate: React.PropTypes.bool
 };
 
 
 Map.defaultProps = {
     ...LeafletMap.defaultProps,
+	locate: false
 };
 
 
