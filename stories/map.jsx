4f4bac466b10cbb00cdf44f2317c4afcc5044a3a
Guillaume AMAT <guillaume.amat@timeonegroup.com>
2017-04-25 09:02:54 +0200

    Add a knob to the map story

--- stories/map.jsx
+++ stories/map.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { host } from 'storybook-host';
-import { withKnobs } from '@kadira/storybook-addon-knobs';
+import { withKnobs, number } from '@kadira/storybook-addon-knobs';
 import { TileLayer } from 'react-leaflet';
 
 import defaultHostOptions from './defaultHostOptions';
 import KnobsAlert from './knobsAlert';
 
+import DefaultTheme from '../themes/default';
 import Map from '../components/map';
 
 
@@ -18,18 +19,23 @@ storiesOf('Map', module)
         height: 450,
     }))
     .addWithInfo('Default state', () => {
+        const zoom = number('Zoom', 13, {
+            range: true,
+            min: 1,
+            max: 18,
+            step: 1,
+        });
         const position = [ 51.505, -0.09 ];
-        const style = { height: 450 };
 
         return (
-            <div>
+            <DefaultTheme>
                 <KnobsAlert />
-                <Map center={position} zoom={13} style={style}>
+                <Map center={position} zoom={zoom} style={{ height: 450 }}>
                     <TileLayer
                         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
                 </Map>
-            </div>
+            </DefaultTheme>
         );
     });
