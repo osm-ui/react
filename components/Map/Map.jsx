dd6fe8f8df18e43e1edf65ee3e39ae6feaa609f6
Guillaume AMAT <guillaume@amat.io>
2017-05-01 17:56:30 +0200

    Improve the locate control handling (and the MapComponent)

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,28 +1,52 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import L from 'leaflet';
 import { Map as LeafletMap } from 'react-leaflet';
 import 'leaflet.locatecontrol';
 
 
-class Map extends LeafletMap {
-    componentDidMount() {
-        super.componentDidMount();
-
-        if (this.props.locate === true) {
-            L.control.locate().addTo(this.leafletElement);
+class Map extends React.Component {
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.locate === true) {
+            this.locateControl = L.control.locate();
+            this.map.addControl(this.locateControl);
         }
+        else if (this.locateControl) {
+            this.map.removeControl(this.locateControl);
+        }
+    }
+
+    handleRef(map) {
+        if (map !== null) {
+            this.map = map.leafletElement;
+        }
+    }
+
+    render() {
+        const {
+            children,
+            ...rest
+        } = this.props;
+
+        return (
+            <LeafletMap
+                ref={map => this.handleRef(map)}
+                {...rest}
+            >
+                {children}
+            </LeafletMap>
+        );
     }
 }
 
 
 Map.propTypes = {
-    ...LeafletMap.propTypes,
     locate: PropTypes.bool,
+    children: PropTypes.node.isRequired,
 };
 
 
 Map.defaultProps = {
-    ...LeafletMap.defaultProps,
     locate: false,
 };
 
--- stories/Map.jsx
+++ stories/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { host } from 'storybook-host';
-import { withKnobs, number } from '@kadira/storybook-addon-knobs';
+import { withKnobs, number, boolean } from '@kadira/storybook-addon-knobs';
 import { TileLayer } from 'react-leaflet';
 
 import defaultHostOptions from '../.storybook/defaultHostOptions';
@@ -21,18 +21,11 @@ storiesOf('Map', module)
         height: 450,
     }))
     .addWithInfo('Default state', () => {
-        const zoom = number('Zoom', 13, {
-            range: true,
-            min: 1,
-            max: 18,
-            step: 1,
-        });
         const position = [ 51.505, -0.09 ];
 
         return (
             <DefaultTheme>
-                <KnobsAlert />
-                <Map center={position} zoom={zoom} style={{ height: 450 }}>
+                <Map center={position} zoom={13} style={{ height: 450 }}>
                     <TileLayer
                         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -46,11 +39,40 @@ storiesOf('Map', module)
         const style = { height: 450 };
 
         return (
-            <Map center={position} zoom={13} style={style} locate>
-                <TileLayer
-                    url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </Map>
+            <DefaultTheme>
+                <Map center={position} zoom={13} style={style} locate>
+                    <TileLayer
+                        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </Map>
+            </DefaultTheme>
+        );
+    })
+    .addWithInfo('Playground', () => {
+        const zoom = number('Zoom', 13, {
+            range: true,
+            min: 1,
+            max: 18,
+            step: 1,
+        });
+        const locate = boolean('Locate control');
+        const position = [ 51.505, -0.09 ];
+
+        return (
+            <DefaultTheme>
+                <KnobsAlert />
+                <Map
+                    center={position}
+                    zoom={zoom}
+                    locate={locate}
+                    style={{ height: 450 }}
+                >
+                    <TileLayer
+                        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </Map>
+            </DefaultTheme>
         );
     });
