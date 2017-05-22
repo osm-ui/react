b8f0f45769f43e17da47cd7b9a134f8170fd0de4
Guillaume AMAT <guillaume@amat.io>
2017-05-22 11:09:15 +0200

    Add all the react-leaflet components to Map

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,3 +1,35 @@
+import * as ReactLeaflet from 'react-leaflet';
 import Map from './Map';
+
+const reactLeafletComponents = [
+    'AttributionControl',
+    'Circle',
+    'CircleMarker',
+    'FeatureGroup',
+    'GeoJSON',
+    'GridLayer',
+    'ImageOverlay',
+    'LayerGroup',
+    'LayersControl',
+    'MapComponent',
+    'MapControl',
+    'MapLayer',
+    'Marker',
+    'Pane',
+    'Path',
+    'Polygon',
+    'Polyline',
+    'Popup',
+    'Rectangle',
+    'ScaleControl',
+    'TileLayer',
+    'Tooltip',
+    'WMSTileLayer',
+    'ZoomControl',
+];
+
+for (const componentName of reactLeafletComponents) {
+    Map[componentName] = ReactLeaflet[componentName];
+}
 
 export default Map;
