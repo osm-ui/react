44bfb1be774821c1d5bb7d7026630b50ef0bd7e7
Guillaume AMAT <guillaume@amat.io>
2017-06-25 14:34:23 +0200

    AppCanvas renamed to Section

--- stories/components/FakeApp.jsx
+++ stories/components/FakeApp.jsx
@@ -3,17 +3,13 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Lorem from 'react-lorem-component';
 import { TextGround } from 'text-ground-react';
-import AppCanvas from '../../components/AppCanvas';
+import Section from '../../components/Section';
 
 
-const StyledAppCanvas = styled(AppCanvas)`
-    height: 80vh;
+const StyledSection = styled(Section)`
+    height: 80vh !important;
     border: 1px solid #ccc;
     padding: 20px 40px;
-`;
-
-const Content = styled.section`
-    opacity: ${props => (props.dimmed ? 0.2 : 1)};
 `;
 
 
@@ -23,13 +19,13 @@ const FakeApp = ({
     children,
     ...rest
 }) => (
-    <StyledAppCanvas {...rest}>
-        <Content dimmed={dimmed}>
+    <StyledSection appCanvas {...rest}>
+        <Section dimmed={dimmed}>
             <h2>{ fakeText ? <TextGround rows={1} /> : 'Application Content' }</h2>
             { fakeText ? <TextGround rows={10} /> : <Lorem count={2} /> }
-        </Content>
+        </Section>
         {children}
-    </StyledAppCanvas>
+    </StyledSection>
 );
 
 
