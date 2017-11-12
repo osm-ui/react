72f3690c77e18175d93dbc46b436676ef5aa34d6
Guillaume AMAT <guillaume@amat.io>
2017-11-13 00:10:31 +0100

    test: Remove useless tests

--- src/components/Loader/Loader.test.jsx
+++ src/components/Loader/Loader.test.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import {
-  snapshotWithElementChildren,
-  snapshotWithTextChildren
-} from '../../helpers/tests';
+import { snapshotWithElementChildren } from '../../helpers/tests';
 import Loader from './';
 
 describe('When using snapshots', () => {
   it('Should render with an element children', () =>
     snapshotWithElementChildren(Loader));
-
-  it('Should render with a text children', () =>
-    snapshotWithTextChildren(Loader));
 });
 
 describe('When using snapshots with a label prop', () => {
   it('Should render with an element children', () =>
     snapshotWithElementChildren(Loader, { label: 'This is a label' }));
-
-  it('Should render with a text children', () =>
-    snapshotWithTextChildren(Loader, { label: 'This is a label' }));
 });
